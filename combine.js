c91cd36df1f09293ae113a9c2b4e2ccb8b5ee465
vuillaume <vitalvuillaume@proton.me>
2025-05-28 22:39:32 +0200

    setup

--- combine.js
+++ combine.js
@@ -1,25 +1,32 @@
 const secrets = require('secrets.js-grempe');
 const fs = require('fs');
-
-
-
-
-
-
-
+const prompt = require("prompt-sync")();
+ 
+ 
+ 
+ 
+ 
 const data = fs.readFileSync('./fragments.json', 'utf8');
-
+ 
 const shares = JSON.parse(data);
-
-
-
-
-
-
+ 
+ 
+ 
+ 
+ 
 function hexToString(hex) {
     return Buffer.from(hex, 'hex').toString('utf8');
 }
-
-const reconstructedSeed = hexToString(secrets.combine([shares[0], shares[1]]));
-
+ 
+let thresholdNb;
+ 
+do {
+    thresholdNb = prompt("Write your threshold numbers for combine : ");
+} while(thresholdNb > shares.length);
+ 
+ 
+const selectedShares = shares.slice(0, thresholdNb);
+const reconstructedSeed = hexToString(secrets.combine(selectedShares));
+ 
+ 
 console.log("\nSeed reconstruite :", reconstructedSeed);
